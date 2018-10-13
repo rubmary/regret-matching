0f0923150f551facddfbea1bb6b5eb9e6d8d88f1
rubmary <rubmaryrojas@gmail.com>
2018-10-13 11:55:37 -0400

    add functions

--- regret-matching.cpp
+++ regret-matching.cpp
@@ -10,18 +10,39 @@ double get_random() {
     return rand() / ((double) RAND_MAX);
 }
 
-int get_action(vector <double> &strategy) {
+int get_action(vector <double> &strategy, int T) {
     double r = get_random();
-    int num_actions = strategy.size();
+    T = strategy.size();
     int action = 0;
     double cumulative_probability = 0;
-    while (action < num_actions - 1) {
+    while (action < T - 1) {
         cumulative_probability += strategy[action];
         if (r < cumulative_probability)
             break;
         action++;
     }
     return action;
+}
+
+void get_strategy(vector <double> &regret, vector <double> &strategy, int T) {
+    double total_regret = 0;
+    for (int k = 0; k < T; k++)
+        total_regret += max(0.0, regret[k]);
+    for (int k = 0; k < T; k++) {
+        strategy[k] = max(0.0, regret[k]);
+        if (total_regret < EPS)
+            strategy[k] = 1/T;
+        else
+            strategy[k] /= total_regret;
+    }
+}
+
+void normalize_strategy(vector <double> &strategy, int T) {
+    double cumulative = 0;
+    for (int k = 0; k < T; k++)
+        cumulative += strategy[k];
+    for (int k = 0; k < T; k++)
+        strategy[k] /= cumulative;
 }
 
 void proc_c(    vector<vector <double>> &utility,   // matriz de pagos
@@ -36,34 +57,12 @@ void proc_c(    vector<vector <double>> &utility,   // matriz de pagos
     s1.resize(S1, 0), s2.resize(S2, 0);                 // inicializar los vectores estrategias
 
     for (int t = 0; t < iterations; t++) {
-        // Calcular la estrategia para el jugador 1
-        double total_regret1 = 0;
-        for (int k = 0; k < S1; k++)
-            total_regret1 += max(0.0, cumulative_regret1[k]);
-        for (int k = 0; k < S1; k++) {
-            strategy1[k] = max(0.0, cumulative_regret1[k]);
-            if (total_regret1 < EPS)
-                strategy1[k] = 1/S1;
-            else
-                strategy1[k] /= total_regret1;
-        }
+        get_strategy(cumulative_regret1, strategy1, S1);    // estrategia del jugador 1
+        get_strategy(cumulative_regret2, strategy2, S2);    // estrategia del jugador 2
+        int i = get_action(strategy1, S1);                  // accion del jugador 1
+        int j = get_action(strategy2, S2);                  // accion del jugador 2
 
-        // Calcular la estrategia para el jugador 2
-        double total_regret2 = 0; 
-        for (int k = 0; k < S2; k++)
-            total_regret2 += max(0.0, cumulative_regret2[k]);
-        for (int k = 0; k < S2; k++) {
-            strategy2[k] = max(0.0, cumulative_regret2[k]);
-            if (total_regret2 < EPS)
-                strategy2[k] = 1/S2;
-            else
-                strategy2[k] /= total_regret2;
-        }
-
-        // Elegir la estrategia para ambos jugadores
-        int i = get_action(strategy1), j = get_action(strategy2);
-
-        // Actualiar los regrets acumulados
+        // Actualizar los regrets acumulados
         for (int k = 0; k < S1; k++)
             cumulative_regret1[k] +=  utility[k][j] - utility[i][j];
         for (int k = 0; k < S2; k++)
@@ -75,19 +74,8 @@ void proc_c(    vector<vector <double>> &utility,   // matriz de pagos
         for (int k = 0; k < S2; k++)
             s2[k] += strategy2[k];
     }
-    // Normalizar la estrategia del jugador 1
-    double sum1 = 0;
-    for (int k = 0; k < S1; k++)
-        sum1 += s1[k];
-    for (int k = 0; k < S1; k++)
-        s1[k] /= sum1;
-
-    // Normalizar la estrategia del jugador 2
-    double sum2 = 0;
-    for (int k = 0; k < S2; k++)
-        sum2 += s2[k];
-    for (int k = 0; k < S2; k++)
-        s2[k] /= sum2; 
+    normalize_strategy(s1, S1);                             // normalizar estrategia jugador 1
+    normalize_strategy(s2, S2);                             // normalizar estrategia jugador 2
 }
 
 int main() {
