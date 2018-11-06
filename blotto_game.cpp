048aee1b81514c4a918fafce6786511430776a9c
rubmary <rubmaryrojas@gmail.com>
2018-11-06 14:21:30 -0400

    blotto game

---
+++ blotto_game.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool next(vector <int> &P, int N, int S) {
+    for (int i = N-1; i > 0; i--) {
+        if (P[i] > 0){
+            P[i-1]++;
+            P[N-1] = P[i] - 1;
+            if (i < N-1) P[i] = 0;
+            return true;
+        }
+    }
+    return false;
+}
+int match(vector <int> P1, vector <int> P2, int N) {
+    int p1 = 0, p2 = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (P1[i] > P2[i])
+            p1++;
+        if (P2[i] > P1[i])
+            p2++;
+    }
+    return p1 - p2;
+}
+int main() {
+    int N, S;
+    cin >> N >> S;
+    vector <int> P1(N, 0), P2(N, 0);
+    P1[N-1] = S;
+    while (true) {
+        P2[0]   = 0;
+        P2[N-1] = S;
+        while (true) {
+            cout << match(P1, P2, N) << ' ';
+            if (!next(P2, N, S))
+                break;
+        }
+        cout << endl << endl;
+        if (!next(P1, N, S))
+            break;
+    }
+}
