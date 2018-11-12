94ece845c997c966425c6f881a5fe970d054b6a7
rubmary <rubmaryrojas@gmail.com>
2018-11-11 21:32:56 -0400

    script kuhn poker

--- src/test_kuhn.cpp
+++ src/test_kuhn.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 #include "RegretMatching.cpp"
 using namespace std;
@@ -7,6 +8,7 @@ void print_strategy(RegretMatching &regret_matching, int S1, int S2) {
     vector <double> s1, s2;
     s1 = regret_matching.get_strategy(1);
     s2 = regret_matching.get_strategy(2);
+    cout << S1 << ' ' << S2 << endl;
     for (int i = 0; i < S1; i++)
         cout << fixed << setprecision(3) << s1[i] << ' ';
     cout << endl;
@@ -16,14 +18,16 @@ void print_strategy(RegretMatching &regret_matching, int S1, int S2) {
 }
 
 int main() {
+    ifstream file("../juegos/forma_normal/tree_kuhn.txt");
     vector <vector <double>> A;
     int S1, S2;
-    cin >> S1 >> S2;
+    file >> S1 >> S2;
     A = vector <vector <double> >(S1, vector <double>(S2));
     for (int i = 0; i < S1; i++)
         for (int j = 0; j < S2; j++)
-            cin >> A[i][j];
+            file >> A[i][j];
 
+    RegretMatching regret_matching;
     regret_matching.proc_c(A, 1000000, "../regret/kuhn/regret.txt");
     print_strategy(regret_matching, S1, S2);
     cout << endl;
