023455b617255d0d9a8f663cfb0d1cfebce092b9
rubmary <rubmaryrojas@gmail.com>
2018-12-26 13:50:58 -0400

    convert identation to spaces

--- src/domino.cpp
+++ src/domino.cpp
@@ -10,48 +10,48 @@ int dy[] = {1, 1, 1, 1, 0, 0, 0};
 
 int fx(int k)
 {
-	return k/C;
+    return k/C;
 }
 
 int fy(int k)
 {
-	return k%C;
+    return k%C;
 }
 
 bool domino(int k, int i, int j)
 {
-	if (x[k] == i && y[k] == j)
-		return true;
+    if (x[k] == i && y[k] == j)
+        return true;
 
-	if (x[k]+dx[k] == i && y[k]+dy[k] == j)
-		return true;
+    if (x[k]+dx[k] == i && y[k]+dy[k] == j)
+        return true;
 
-	return false;
+    return false;
 }
 
 bool ficha(int k, int i, int j)
 {
-	if (fx(k) == i && fy(k) == j)
-		return true;
-	return false;
+    if (fx(k) == i && fy(k) == j)
+        return true;
+    return false;
 }
 
 int match (int i, int j) {
-	for (int r = 0; r < R; r++) {
-		for (int c = 0; c < C; c++)
-			if (domino(i, r, c) && ficha(j, r, c))
-				return -1;
-	}
-	return 1;
+    for (int r = 0; r < R; r++) {
+        for (int c = 0; c < C; c++)
+            if (domino(i, r, c) && ficha(j, r, c))
+                return -1;
+    }
+    return 1;
 }
 
 int N = 7, M = 6;
 int main() {
-	cout << N << ' ' << M << endl;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cout << match(i, j) << ' ';
-		}
-		cout << endl;
-	}
+    cout << N << ' ' << M << endl;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << match(i, j) << ' ';
+        }
+        cout << endl;
+    }
 }
--- src/graphics.py
+++ src/graphics.py
@@ -2,34 +2,34 @@ import matplotlib.pylab as plt
 import sys
 
 def make_plot(path_input, path_graph, proc):
-	file = open(path_input)
-	lines = file.readlines()
-	file.close()
-	regret1 = [float(lines[i].split()[0]) for i in range(len(lines))]
-	regret2 = [float(lines[i].split()[1]) for i in range(len(lines))]
-	plt.figure()
-	plt.title('Procedimiento ' + proc)
-	plt.xscale('log')
-	plt.plot(regret1)
-	plt.plot(regret2)
-	plt.ylabel('Regret (max)')
-	plt.xlabel('Iteraciones')
-	plt.savefig(path_graph)
+    file = open(path_input)
+    lines = file.readlines()
+    file.close()
+    regret1 = [float(lines[i].split()[0]) for i in range(len(lines))]
+    regret2 = [float(lines[i].split()[1]) for i in range(len(lines))]
+    plt.figure()
+    plt.title('Procedimiento ' + proc)
+    plt.xscale('log')
+    plt.plot(regret1)
+    plt.plot(regret2)
+    plt.ylabel('Regret (max)')
+    plt.xlabel('Iteraciones')
+    plt.savefig(path_graph)
 
 if __name__ == "__main__":
-	name = sys.argv[1]
-	path_input_list = list('../regret/'   + name + '/procedimiento- ')
-	path_graph_list = list('../graficas/' + name + '/procedimiento- ')
-	pos_input = len(path_input_list) - 1
-	pos_graph = len(path_graph_list) - 1
+    name = sys.argv[1]
+    path_input_list = list('../regret/'   + name + '/procedimiento- ')
+    path_graph_list = list('../graficas/' + name + '/procedimiento- ')
+    pos_input = len(path_input_list) - 1
+    pos_graph = len(path_graph_list) - 1
 
-	path_input_list = path_input_list + list('.txt')
-	path_graph_list = path_graph_list + list('.png')
-	procs = ['A', 'B', 'C']
-	for i in procs:
-		path_input_list[pos_input] = i
-		path_graph_list[pos_graph] = i
-		path_input = ''.join(path_input_list)
-		path_graph = ''.join(path_graph_list)
-		make_plot(path_input, path_graph, i)
+    path_input_list = path_input_list + list('.txt')
+    path_graph_list = path_graph_list + list('.png')
+    procs = ['A', 'B', 'C']
+    for i in procs:
+        path_input_list[pos_input] = i
+        path_graph_list[pos_graph] = i
+        path_input = ''.join(path_input_list)
+        path_graph = ''.join(path_graph_list)
+        make_plot(path_input, path_graph, i)
 
